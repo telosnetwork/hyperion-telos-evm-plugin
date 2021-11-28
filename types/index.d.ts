ad5762d26def1f64e088914443b4abebd447a8fb
Jesse Schulman <jesse@caleos.io>
2021-11-27 21:23:08 -0800

    Indexer websocket

---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+export interface TelosEvmConfig {
+    signer_account: string;
+    signer_permission: string;
+    signer_key: string;
+    contracts: {
+        main: string;
+    }
+    chainId: number;
+    debug: boolean;
+    indexerWebsocketHost: string;
+    indexerWebsocketPort: number;
+    indexerWebsocketUri: string;
+    rpcWebsocketHost: string;
+    rpcWebsocketPort: number;
+}
---
+++ ws/RawActionBroadcaster.ts
@@ -0,0 +1,42 @@
+import uWS, {TemplatedApp} from "uWebSockets.js";
+import {TelosEvmConfig} from "../types";
+
+export default class RawActionBroadcaster {
+
+    config: TelosEvmConfig
+    broadcastServer: TemplatedApp
+
+    constructor(config: TelosEvmConfig) {
+        this.config = config;
+        this.initUWS();
+    }
+
+    initUWS() {
+        this.broadcastServer = uWS.App({
+        }).ws('/evm', {
+            compression: 0,
+            maxPayloadLength: 16 * 1024 * 1024,
+            /* We need a slightly higher timeout for this crazy example */
+            idleTimeout: 60,
+            open: (ws) => ws.subscribe('raw'),
+            message: () => {},
+            drain: () => {},
+            close: () => {},
+        }).listen(, (token) => {
+            if (token) {
+                console.log('Listening to port ' + port);
+            } else {
+                console.log('Failed to listen to port ' + port);
+            }
+        });
+    }
+
+    broadcastRaw(rawAction) {
+        if (!this.broadcastServer) {
+            console.error("RawActionBroadcaster.broadcastRaw was called before broadcastServer was set");
+            return;
+        }
+
+        this.broadcastServer.publish('raw', JSON.stringify(rawAction));
+    }
+}
