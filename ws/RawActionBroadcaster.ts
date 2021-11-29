bc83e15357fb62c96e8f03dcb522c4603ddc3554
Jesse Schulman <jesse@caleos.io>
2021-11-28 21:51:15 -0800

    Using the handler map for plugin messages

--- ws/RawActionBroadcaster.ts
+++ ws/RawActionBroadcaster.ts
@@ -9,7 +9,6 @@ export default class RawActionBroadcaster {
     constructor(config: TelosEvmConfig) {
         this.config = config;
         this.initUWS();
-        this.setupIPC();
     }
 
     initUWS() {
@@ -32,17 +31,6 @@ export default class RawActionBroadcaster {
                 console.log('Failed to listen to port ' + port);
             }
         });
-    }
-
-    setupIPC() {
-        process.on('message', (msg: any) => {
-            switch (msg.event) {
-                case 'evm_transaction': {
-                    console.log("RECEIVED EVM TRX: " + JSON.stringify(msg));
-                    break;
-                }
-            }
-        })
     }
 
     broadcastRaw(rawAction) {
