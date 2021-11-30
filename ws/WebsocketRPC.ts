ac1385aad3e1423eb3d03c7ada27d7fe9d1edde0
Jesse Schulman <jesse@caleos.io>
2021-11-29 16:55:05 -0800

    Set clientInfo and properly toString on the ArrayBuffers

--- ws/WebsocketRPC.ts
+++ ws/WebsocketRPC.ts
@@ -8,13 +8,13 @@ export default class WebsocketRPC {
     config: TelosEvmConfig
     websocketRPC: TemplatedApp
     websocketClient: WebSocket
-    rpcHandler: any
+    rpcHandlerContainer: any
 
-    constructor(config: TelosEvmConfig, rpcHandler: any) {
+    constructor(config: TelosEvmConfig, rpcHandlerContainer: any) {
         this.config = config;
         this.initUWS();
         this.initWSClient();
-        this.rpcHandler = rpcHandler;
+        this.rpcHandlerContainer = rpcHandlerContainer;
     }
 
     initWSClient() {
@@ -32,10 +32,8 @@ export default class WebsocketRPC {
             maxPayloadLength: 16 * 1024 * 1024,
             open: () => {
             },
-            message: async (msg) => {
-                const rpcResponse = await this.rpcHandler(msg);
-                console.log("GOT RESPONSE: ");
-                console.dir(rpcResponse);
+            message: (ws, msg) => {
+                this.handleMessage(ws, msg);
             },
             drain: () => {
             },
@@ -48,6 +46,28 @@ export default class WebsocketRPC {
                 console.log('Failed to listen to port ' + port);
             }
         });
+    }
+
+    async handleMessage(ws, msg) {
+        const buffer = Buffer.from(msg);
+        const string = buffer.toString();
+        try {
+            const msgObj = JSON.parse(string);
+            if (!msgObj.method) {
+                ws.send({"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request, no method specified"}, "id": msgObj.id ? msgObj.id : null})
+                return;
+            }
+
+            const ip = Buffer.from(ws.getRemoteAddressAsText()).toString();
+            const origin = "Websocket Client";
+            const usage = 0;
+            const limit = 0;
+            const clientInfo = { ip, origin, usage, limit };
+            const rpcResponse = await this.rpcHandlerContainer.handler(msg, clientInfo);
+            ws.send(JSON.stringify(rpcResponse));
+        } catch (e) {
+            console.error(`Failed to parse websocket message: ${string} error: ${e.message}`);
+        }
     }
 
     handleIndexerMessage(data) {
